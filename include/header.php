0e722042ea2ed34bd0605147ad0aeb52387cb57d
Francisny03 <120179564+Francisny03@users.noreply.github.com>
2024-05-03 14:46:22 +0100

    css

--- include/header.php
+++ include/header.php
@@ -1,9 +1,31 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Montserrat:ital,
+        wght@0,100..900;1,100..900&display=swap" rel="stylesheet">
+    <link rel="stylesheet" href="..CSS/style.css">
     <title>Martin Coiffure</title>
 </head>
+
 <body>
-    
+    <header>
+        <div class="logo_container">
+            <a href="index.php">
+                <img src="Images/logo_martin.png" alt="logo Martin Coiffure">
+            </a>
+        </div>
+        <nav class="menu_nav">
+            <ul>
+                <li class="btn_nav active"><a href="index.php">Accueil</a></li>
+                <li class="btn_nav"><a href="conseils.html">Services</a></li>
+                <li class="btn_nav"><a href="divers.html">Galerie</a></li>
+                <li class="btn_nav"><a href="qui_sommes_nous.html">A propos</a></li>
+                <li class="btn_nav"><a href="contact.html">Contact</a></li>
+            </ul>
+        </nav>
+    </header>
