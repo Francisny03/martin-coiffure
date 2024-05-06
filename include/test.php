b962d9419acae75d88fd1dd82582baf8f66c1781
khalioba <123170729+khalioba@users.noreply.github.com>
2024-05-06 15:43:12 +0100

    test

---
+++ include/test.php
@@ -0,0 +1,36 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Montserrat:ital,
+        wght@0,100..900;1,100..900&display=swap" rel="stylesheet">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/slick-carousel/1.8.0/slick.css" integrity="sha512-wR4oNhLBHf7smjy0K4oqzdWumd+r5/+6QO/vDda76MW5iug4PT7v86FoEkySIJft3XA0Ae6axhIvHrqwm793Nw==" crossorigin="anonymous" referrerpolicy="no-referrer" />
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/slick-carousel/1.8.0/slick-theme.css" integrity="sha512-6lLUdeQ5uheMFbWm3CP271l14RsX1xtx+J5x2yeIDkkiBpeVTNhTqijME7GgRKKi6hCqovwCoBTlRBEC20M8Mg==" crossorigin="anonymous" referrerpolicy="no-referrer" />
+    <link rel="stylesheet" href="CSS/tste.css?<?= rand() ?>">
+    <title>Martin Coiffure</title>
+</head>
+
+<body>
+    <div class="menu">
+        <div class="menu_elements space">
+            <div class="logo_container">
+                <a href="index.php">
+                    <img src="Images/logo_martin.png" alt="logo Martin Coiffure">
+                    <p>Martin Coiffure</p>
+                </a>
+            </div>
+            <nav class="menu_nav">
+                <ul>
+                    <li class="btn_nav active"><a href="index.php">Accueil</a></li>
+                    <li class="btn_nav"><a href="conseils.html">Services</a></li>
+                    <li class="btn_nav"><a href="divers.html">Galerie</a></li>
+                    <li class="btn_nav"><a href="qui_sommes_nous.html">A propos</a></li>
+                    <li class="btn_nav"><a href="contact.html">Contact</a></li>
+                </ul>
+            </nav>
+        </div>
+    </div>
---
+++ test.php
@@ -0,0 +1,111 @@
+<?php
+include('include/test.php')
+?>
+
+<div class="slide">
+    <div class="slide_item">
+        <img src="Images/slider_1.webp" alt="">
+        <div class="slide-text">
+            <h1>Vous êtes la bienvenue au salon Martin Coiffure</h1>
+            <h3>Laissez nos experts vous offrir une expérience de coiffure personnalisée...</h3>
+        </div>
+    </div>
+    <div class="slide_item">
+        <img src="Images/slider_2.webp" alt="">
+        <div class="slide-text">
+            <h1>Vous êtes la bienvenue au salon Martin Coiffure</h1>
+            <h3>Laissez nos experts vous offrir une expérience de coiffure personnalisée...</h3>
+        </div>
+    </div>
+    <div class="slide_item">
+        <img src="Images/slider_3.webp" alt="">
+        <div class="slide-text">
+            <h1>Vous êtes la bienvenue au salon Martin Coiffure</h1>
+            <h3>Laissez nos experts vous offrir une expérience de coiffure personnalisée...</h3>
+        </div>
+    </div>
+    <div class="slide_item">
+        <img src="Images/slider_4.webp" alt="">
+        <div class="slide-text">
+            <h1>Vous êtes la bienvenue au salon Martin Coiffure</h1>
+            <h3>Laissez nos experts vous offrir une expérience de coiffure personnalisée...</h3>
+        </div>
+    </div>
+</div>
+
+<br>
+<div class="nos_services_bloc space">
+    <div class="nos_services_title arrow">
+        <p class="form_bf_title service_title space">
+            Nos services
+        </p>
+        <img src="Images/arrow.svg" alt="">
+    </div>
+    <br>
+    <div class="all_services">
+        <div class="nos_services_item">
+            <div class="rectangle rect_img1">
+                <p>
+                    Coiffure Dames
+                </p>
+            </div>
+        </div>
+        <div class="nos_services_item">
+            <div class="rectangle rect_img2">
+                <p>
+                    Centre de formation
+                </p>
+            </div>
+        </div>
+        <div class="nos_services_item">
+            <div class="rectangle rect_img3">
+                <p>
+                    Décoration de voitures
+                </p>
+            </div>
+        </div>
+        <div class="nos_services_item">
+            <div class="rectangle rect_img4">
+                <p>
+                    Make-up
+                </p>
+            </div>
+        </div>
+        <div class="nos_services_item">
+            <div class="rectangle rect_img5">
+                <p>
+                    Manucure & Pédicure
+                </p>
+            </div>
+        </div>
+        <div class="nos_services_item">
+            <div class="rectangle rect_img6">
+                <p>
+                    Coloration capillaire
+                </p>
+            </div>
+        </div>
+    </div>
+
+</div>
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+<?php
+include('include/footer.php')
+?>
