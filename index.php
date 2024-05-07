13a6c00313002d7277c02e02dd5bfc05f7e13ae9
Francisny03 <120179564+Francisny03@users.noreply.github.com>
2024-05-07 18:04:08 +0100

    chef

--- index.php
+++ index.php
@@ -33,6 +33,7 @@ include('include/header.php')
     </div>
 </div>
 <br>
+<br>
 <div class="nos_services_bloc space">
     <div class="nos_services_title arrow">
         <p class="form_bf_title service_title space">
@@ -43,46 +44,215 @@ include('include/header.php')
     <br>
     <div class="all_services">
         <div class="nos_services_item">
-            <div class="rectangle rect_img1">
-                <p>
-                    Coiffure Dames
+            <div class="services">
+                <div class="rectangle rect_img1">
+                    <p>
+                        Coiffure Dames
+                    </p>
+                </div>
+            </div>
+            <div class="services">
+                <div class="rectangle rect_img2">
+                    <p>
+                        Centre de formation
+                    </p>
+                </div>
+            </div>
+            <div class="services">
+                <div class="rectangle rect_img3">
+                    <p>
+                        Décoration de voitures
+                    </p>
+                </div>
+            </div>
+            <div class="services">
+                <div class="rectangle rect_img4">
+                    <p>
+                        Make-up
+                    </p>
+                </div>
+            </div>
+            <div class="services">
+                <div class="rectangle rect_img5">
+                    <p>
+                        Manucure & Pédicure
+                    </p>
+                </div>
+            </div>
+            <div class="services">
+                <div class="rectangle rect_img6">
+                    <p>
+                        Coloration capillaire
+                    </p>
+                </div>
+            </div>
+        </div>
+
+    </div>
+
+</div>
+
+<br>
+<br>
+
+<div class="nos_services_bloc space">
+    <div class="nos_services_title arrow">
+        <p class="form_bf_title service_title space">
+            Nos réalisations
+        </p>
+        <img src="Images/arrow.svg" alt="">
+    </div>
+    <br>
+    <div class="all_services">
+        <div class="nos_services_item">
+            <div class="realisation">
+                <div class="realisation_photo">
+                    <img src="Images/real1.webp" alt="">
+                </div>
+                <div class="rectangle_noir">
+                    <div class="rect_noir_text">
+                        <p>
+                            Tissage bouclé
+                        </p>
+                    </div>
+                </div>
+            </div>
+            <div class="realisation">
+                <div class="realisation_photo">
+                    <img src="Images/real2.webp" alt="">
+                </div>
+                <div class="rectangle_noir">
+                    <div class="rect_noir_text">
+                        <p>
+                            Tissage bouclé
+                        </p>
+                    </div>
+                </div>
+            </div>
+            <div class="realisation">
+                <div class="realisation_photo">
+                    <img src="Images/real3.webp" alt="">
+                </div>
+                <div class="rectangle_noir">
+                    <div class="rect_noir_text">
+                        <p>
+                            Tissage bouclé
+                        </p>
+                    </div>
+                </div>
+            </div>
+            <div class="realisation">
+                <div class="realisation_photo">
+                    <img src="Images/real4.webp" alt="">
+                </div>
+                <div class="rectangle_noir">
+                    <div class="rect_noir_text">
+                        <p>
+                            Tissage bouclé
+                        </p>
+                    </div>
+                </div>
+            </div>
+            <div class="realisation">
+                <div class="realisation_photo">
+                    <img src="Images/real5.webp" alt="">
+                </div>
+                <div class="rectangle_noir">
+                    <div class="rect_noir_text">
+                        <p>
+                            Tissage bouclé
+                        </p>
+                    </div>
+                </div>
+            </div>
+            <div class="realisation">
+                <div class="realisation_photo">
+                    <img src="Images/real6.webp" alt="">
+                </div>
+                <div class="rectangle_noir">
+                    <div class="rect_noir_text">
+                        <p>
+                            Tissage bouclé
+                        </p>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+
+<br>
+<br>
+
+<div class="nos_services_bloc space">
+    <div class="nos_services_title arrow">
+        <p class="form_bf_title service_title space">
+            Galerie
+        </p>
+        <img src="Images/arrow.svg" alt="">
+    </div>
+</div>
+<br>
+<div class="slider_video">
+    <div class="filter">
+        <div class="slider_video_text">
+            <p>Regarder nos vidéos sur notre chaîne Youtube</p>
+            <img src="Images/play.svg" alt="">
+        </div>
+    </div>
+</div>
+<br>
+
+<div class="galerie space">
+    <div class="galerie_photo">
+        <img src="Images/real1.webp" alt="">
+    </div>
+    <div class="galerie_photo">
+        <img src="Images/real1.webp" alt="">
+    </div>
+    <div class="galerie_photo">
+        <img src="Images/real1.webp" alt="">
+    </div>
+    <div class="galerie_photo">
+        <img src="Images/real1.webp" alt="">
+    </div>
+    <div class="galerie_photo">
+        <img src="Images/real1.webp" alt="">
+    </div>
+    <div class="galerie_photo">
+        <img src="Images/real1.webp" alt="">
+    </div>
+</div>
+
+<br>
+<br>
+
+<div class="nos_services_bloc space">
+    <div class="nos_services_title arrow">
+        <p class="form_bf_title service_title space">
+            Mot du chef
+        </p>
+    </div>
+</div>
+<br>
+<div class="chef_rectangle">
+    <div class="chef_text_image space">
+        <div class="chef_text">
+            <div class="chef_text_grif_name">
+                <div class="chef_text_griffes"></div>
+                <div class="chef_text_name">Martin ITOUA</div>
+            </div>
+            <br>
+            <br>
+            <div class="chef_text_bas">
+                <p>La coiffure est notre passion, faire de votre instant coiffure un moment
+                    de détente pour vous, une véritable pause, afin de vous rendre unique...
+                    Nous vous souhaitons la bienvenue au Salon Martin Coiffure...
                 </p>
             </div>
         </div>
-        <div class="nos_services_item">
-            <div class="rectangle rect_img2">
-                <p>
-                    Centre de formation
-                </p>
-            </div>
-        </div>
-        <div class="nos_services_item">
-            <div class="rectangle rect_img3">
-                <p>
-                    Décoration de voitures
-                </p>
-            </div>
-        </div>
-        <div class="nos_services_item">
-            <div class="rectangle rect_img4">
-                <p>
-                    Make-up
-                </p>
-            </div>
-        </div>
-        <div class="nos_services_item">
-            <div class="rectangle rect_img5">
-                <p>
-                    Manucure & Pédicure
-                </p>
-            </div>
-        </div>
-        <div class="nos_services_item">
-            <div class="rectangle rect_img6">
-                <p>
-                    Coloration capillaire
-                </p>
-            </div>
+        <div class="chef_image">
+            <img src="Images/chef.webp" alt="">
         </div>
     </div>
 
@@ -94,9 +264,29 @@ include('include/header.php')
 
 
 
+<br>
+<br>
+<br>
+<br>
+Du texte Du texteDu texteDu texteDu texteDu texteDu texteDu texte
 
+<br>
+<br>
+<br>
+<br>
+Du texte Du texteDu texteDu texteDu texteDu texteDu texteDu texte
 
+<br>
+<br>
+<br>
+<br>
+Du texte Du texteDu texteDu texteDu texteDu texteDu texteDu texte
 
+<br>
+<br>
+<br>
+<br>
+Du texte Du texteDu texteDu texteDu texteDu texteDu texteDu texte
 
 
 
