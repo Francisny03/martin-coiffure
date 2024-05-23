f658dbb96b0dcd447ade5dd1f4fb37cebb13e8bb
Francisny03 <120179564+Francisny03@users.noreply.github.com>
2024-05-23 23:04:23 +0100

    favicon2

--- include/header.php
+++ include/header.php
@@ -9,9 +9,18 @@
     <meta property="og:image" content="https://martincoiffure.top/Images/logo_ref.jpg">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
 
+    <!--Favicon 2  -->
+    <link rel="apple-touch-icon" sizes="180x180" href="Images/favicon/apple-touch-icon.png">
+    <link rel="icon" type="image/png" sizes="32x32" href="Images/favicon/favicon-32x32.png">
+    <link rel="icon" type="image/png" sizes="16x16" href="Images/favicon/favicon-16x16.png">
+    <link rel="manifest" href="Images/favicon/site.webmanifest">
+    <link rel="mask-icon" href="Images/favicon/safari-pinned-tab.svg" color="#5bbad5">
+    <meta name="msapplication-TileColor" content="#da532c">
+    <meta name="theme-color" content="#ffffff">
+
     <!-- Favicon -->
-    <link rel="icon" type="image/ico/x-icon" href="Images/favicon_io/favicon.ico">
-    
+    <!-- <link rel="icon" type="image/ico/x-icon" href="Images/favicon_io/favicon.ico"> -->
+
     <!-- wow animate_animated -->
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/animate.css/4.1.1/animate.min.css" />
     <!-- swiper, slider -->
