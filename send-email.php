4e4e8a446b2822175ee0838e9189fd70e15a1fb4
Francisny03 <120179564+Francisny03@users.noreply.github.com>
2024-05-23 17:31:19 +0100

    php

--- send-email.php
+++ send-email.php
@@ -6,9 +6,9 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
     $ville = htmlspecialchars($_POST['ville']);
     $message = htmlspecialchars($_POST['textarea']);
 
-    $to = "khalilobaadef@gmail.com"; // Remplacez par votre adresse e-mail
+    $to = "contact@martincoiffure.top"; // Remplacez par votre adresse e-mail
     $subject = "Nouveau message de contact";
-    $body = "Prénom: $prenom\nAdresse mail: $mail\nNuméro de téléphone: $number\nVille: $ville\n\nMessage:\n$message";
+    $body = "Prenom: $prenom\nAdresse mail: $mail\nNumero de telphone: $number\n\nMessage:\n$message";
     $headers = "From: $mail";
 
     if (mail($to, $subject, $body, $headers)) {
