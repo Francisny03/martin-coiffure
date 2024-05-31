4a5e086904510cab500a922a3e297261fc575502
Francisny03 <120179564+Francisny03@users.noreply.github.com>
2024-05-31 11:26:46 +0100

    finish

--- galerie_view.php
+++ galerie_view.php
@@ -12,57 +12,57 @@ include('include/header.php')
 
 
 <div class="galerie_view_all_page">
-        <div class="galerie_view_items">
-            <img src="Images/real3.webp" alt="">
-        </div>
-        <div class="galerie_view_items">
-            <img src="Images/real1.webp" alt="">
-        </div>
-        <div class="galerie_view_items">
-            <img src="Images/real2.webp" alt="">
-        </div>
-        <div class="galerie_view_items">
-            <img src="Images/real4.webp" alt="">
-        </div>
-        <div class="galerie_view_items">
-            <img src="Images/real5.webp" alt="">
-        </div>
-        <div class="galerie_view_items">
-            <img src="Images/real6.webp" alt="">
-        </div>
-        <div class="galerie_view_items">
-            <img src="Images/real7.webp" alt="">
-        </div>
-        <div class="galerie_view_items">
-            <img src="Images/real8.webp" alt="">
-        </div>
-        <div class="galerie_view_items">
-            <img src="Images/real9.webp" alt="">
-        </div>
-        <div class="galerie_view_items">
-            <img src="Images/real10.webp" alt="">
-        </div>
-        <div class="galerie_view_items">
-            <img src="Images/real11.webp" alt="">
-        </div>
-        <div class="galerie_view_items">
-            <img src="Images/real12.webp" alt="">
-        </div>
-        <div class="galerie_view_items">
-            <img src="Images/perruque2.webp" alt="">
-        </div>
-        <div class="galerie_view_items">
-            <img src="Images/real2.webp" alt="">
-        </div>
-        <div class="galerie_view_items">
-            <img src="Images/real3.webp" alt="">
-        </div>
-        <div class="galerie_view_items">
-            <img src="Images/formation2.jpg" alt="">
-        </div>
-        <div class="galerie_view_items">
-            <img src="Images/slider_3.webp" alt="">
-        </div>        
+    <div class="galerie_view_items">
+        <img src="Images/real3.webp" alt="">
+    </div>
+    <div class="galerie_view_items">
+        <img src="Images/real1.webp" alt="">
+    </div>
+    <div class="galerie_view_items">
+        <img src="Images/real2.webp" alt="">
+    </div>
+    <div class="galerie_view_items">
+        <img src="Images/real4.webp" alt="">
+    </div>
+    <div class="galerie_view_items">
+        <img src="Images/real5.webp" alt="">
+    </div>
+    <div class="galerie_view_items">
+        <img src="Images/real6.webp" alt="">
+    </div>
+    <div class="galerie_view_items">
+        <img src="Images/real7.webp" alt="">
+    </div>
+    <div class="galerie_view_items">
+        <img src="Images/real8.webp" alt="">
+    </div>
+    <div class="galerie_view_items">
+        <img src="Images/real9.webp" alt="">
+    </div>
+    <div class="galerie_view_items">
+        <img src="Images/real10.webp" alt="">
+    </div>
+    <div class="galerie_view_items">
+        <img src="Images/real11.webp" alt="">
+    </div>
+    <div class="galerie_view_items">
+        <img src="Images/real12.webp" alt="">
+    </div>
+    <div class="galerie_view_items">
+        <img src="Images/perruque2.webp" alt="">
+    </div>
+    <div class="galerie_view_items">
+        <img src="Images/real2.webp" alt="">
+    </div>
+    <div class="galerie_view_items">
+        <img src="Images/real3.webp" alt="">
+    </div>
+    <div class="galerie_view_items">
+        <img src="Images/formation2.jpg" alt="">
+    </div>
+    <div class="galerie_view_items">
+        <img src="Images/slider_3.webp" alt="">
+    </div>
 </div>
 
 
